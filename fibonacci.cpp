33257fdb273ab639003c772fa310301cefb8eb30
BrunoBGrande <80929451+BrunoBGrande@users.noreply.github.com>
2021-05-09 21:26:22 -0300

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>  
+using namespace std;  
+
+int main() {  
+
+  int numero,s1=0,s2=1,s3,i;   
+
+ cout<<"insira o termo desejado da sequencia: ";  
+ cin>>numero;//pedindo informação do usuário    
+ cout<<s1<<" "<<s2<<" "; //"printando" 0 e 1    
+ for(i=2;i<numero;++i) // o loop começa apartir de 2 já que 0 e 1 são préviamente "printados"    
+ {    
+  s3=s1+s2;    
+  cout<<s3<<" ";    
+  s1=s2;    
+  s2=s3;    
+ }   
+}  
